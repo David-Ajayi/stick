2dac9bd2d7c355a1ebd2a5283ef6546d78864f2d
David Ajayi <davidajayi00@gmail.com>
2021-12-02 08:35:28 +0000

    instantiating a fakestore object and making request to the api with check availability()

--- tests/Clients/FakeStoreTest.php
+++ tests/Clients/FakeStoreTest.php
@@ -1,11 +1,40 @@
 <?php
 
 namespace Tests\Clients;
-
 use App\Clients\FakeStore;
-use PHPUnit\Framework\TestCase;
+use App\Models\Stock;
+use Tests\TestCase;
+use RetailerWithProductSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 
+/**
+ * @group api
+ */
 class FakeStoreTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /** @test */
+    function it_tracks_a_product()
+    {
+        $this->seed(RetailerWithProductSeeder::class);
+
+        $stock = tap(Stock::first())->update([
+            'sku' => '11', // fake sku which is the id for this laptop product at the fakestore api
+            'url' => 'https://www.bestbuy.com/site/nintendo-switch-32gb-console-gray-joy-con/6364253.p?skuId=6364253'
+        ]);
+
+        try {
+            (new FakeStore())->checkAvailability($stock);
+        } catch (\Exception $e) {
+//            $this->fail('Failed to track the BestBuy API properly.');
+            $this->fail('Failed to track the FakeStore API properly. ' . $e->getMessage());
+        }
+
+        $this->assertTrue(true);
+    }
+
+
 
 }
